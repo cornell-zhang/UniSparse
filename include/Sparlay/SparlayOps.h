045407e43f5efc4e198a1a6cc825570bde6aaa6c
Sibylau <jl3952@cornell.edu>
2022-01-28 17:35:09 -0500

    [Op] support multi output

--- include/Sparlay/SparlayOps.h
+++ include/Sparlay/SparlayOps.h
@@ -13,6 +13,7 @@
 #include "mlir/IR/Dialect.h"
 #include "mlir/IR/OpDefinition.h"
 #include "mlir/Interfaces/SideEffectInterfaces.h"
+#include "mlir/IR/OpImplementation.h"
 
 #define GET_OP_CLASSES
 #include "Sparlay/SparlayOps.h.inc"
