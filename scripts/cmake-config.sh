f3516bdcdaf66dfc8679b1b6c31c62a28c14c2af
Jie Liu <jl3952@cornell.edu>
2022-02-14 13:33:55 -0500

    Update cmake-config.sh

--- scripts/cmake-config.sh
+++ scripts/cmake-config.sh
@@ -1,6 +1,6 @@
 cmake -G Ninja .. \
--DMLIR_DIR=/work/shared/users/phd/jl3952/installs/MLIR/llvm-project-gpu/build/lib/cmake/mlir \
--DLLVM_EXTERNAL_LIT=/work/shared/users/phd/jl3952/installs/MLIR/llvm-project-gpu/build/bin/llvm-lit \
+-DMLIR_DIR=/work/shared/common/llvm-project-gpu/build/lib/cmake/mlir \
+-DLLVM_EXTERNAL_LIT=/work/shared/common/llvm-project-gpu/build/bin/llvm-lit \
 -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ 
 
 # ninja
