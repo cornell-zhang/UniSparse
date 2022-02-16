dfa44abdddf9dae391d0a7b00ea9987f2cdc11ad
Sibylau <jl3952@cornell.edu>
2022-02-15 20:56:50 -0500

    [Conversion] finish PackOp COO lowering

--- lib/Transforms/LowerFormatConversionPass.cpp
+++ lib/Transforms/LowerFormatConversionPass.cpp
@@ -48,29 +48,23 @@ class PackOpLowering : public OpConversionPattern<sparlay::PackOp> {
 public:
     using OpConversionPattern<sparlay::PackOp>::OpConversionPattern;
 
+    // enum padding_options {"none", "zero"};
+
     LogicalResult 
         matchAndRewrite(sparlay::PackOp op, OpAdaptor adaptor,
                         ConversionPatternRewriter &rewriter) const final {
         Location loc = op->getLoc();
         Value input = op->getOperand(0);
-        
-        // auto reduceDimAttr = op->getAttr("reduce_dim");
-        // auto paddingAttr = op->getAttr("padding");
-        // auto storageOrderAttr = op->getAttr("storage_order");
         auto reduceDim = op.reduce_dim();
         StringRef padding = op.padding();
         AffineMap storageOrder = op.storage_order();
-        LLVM_DEBUG(llvm::dbgs()<< "affinemap: ");
-        LLVM_DEBUG(storageOrder.print(llvm::dbgs()));
-        LLVM_DEBUG(llvm::dbgs() << "  getNumDims: " << storageOrder.getNumDims()
-                    << "  getNumSymbols: " << storageOrder.getNumSymbols() << 
-                    "getNumResults: " << storageOrder.getNumResults() << 
-                    "getNumInputs: " << storageOrder.getNumInputs());
 
         ShapedType inputTp = input.getType().cast<ShapedType>();
         ArrayRef<int64_t> shape = inputTp.getShape();
         Type indexTp = rewriter.getIndexType();
-        auto indexArrTp = MemRefType::get(shape, indexTp);
+        LLVM_DEBUG(llvm::dbgs()<< "shape.size() = " << shape.size() << "\n");
+
+        MemRefType indexArrTp = MemRefType::get(shape, indexTp);
         Value index_arr = rewriter.create<memref::AllocaOp>(loc, indexArrTp);
 
         std::vector<int64_t> reduce_shape;
@@ -81,26 +75,29 @@ public:
             }
         }
 
-        // ArrayRef<int64_t> reduce_shape_arr(reduce_shape);
-        auto nnzPerRowTp = MemRefType::get(ArrayRef<int64_t>(reduce_shape), indexTp);
+        MemRefType nnzPerRowTp = MemRefType::get(ArrayRef<int64_t>(reduce_shape), indexTp);
         Value nnz_per_row = rewriter.create<memref::AllocaOp>(loc, nnzPerRowTp);
 
-        Value zero = rewriter.create<ConstantOp>(loc, rewriter.getIndexAttr(0));
-        Value dim = rewriter.create<memref::DimOp>(loc, input, zero);
-        rewriter.create<linalg::FillOp>(loc, zero, nnz_per_row);
-        rewriter.create<linalg::FillOp>(loc, dim, index_arr);
+        Type inputElmTp = input.getType().cast<MemRefType>().getElementType();
+        Value zeroElm = rewriter.create<ConstantOp>(loc, inputElmTp, rewriter.getZeroAttr(inputElmTp));
 
-        SmallVector<Value> lb_outer, lb;
-        SmallVector<Value> hb_outer, hb;
-        SmallVector<Value> step_outer, step;
+        Value zero = rewriter.create<ConstantOp>(loc, rewriter.getIndexAttr(0));
         Value one = rewriter.create<ConstantOp>(loc, rewriter.getIndexAttr(1));
+        Value dim = rewriter.create<memref::DimOp>(loc, input, zero);
+
+        SmallVector<Value> lb_outer, lb, lb_orig;
+        SmallVector<Value> hb_outer, hb, hb_orig;
+        SmallVector<Value> step_outer, step, step_orig;
         for (unsigned i = 0; i < shape.size(); i++) {
+            Value dimSize = rewriter.create<ConstantOp>(loc, rewriter.getIndexAttr(shape[i]));
             if (i != reduceDimValue) {
                 lb_outer.push_back(zero);
-                Value dimSize = rewriter.create<ConstantOp>(loc, rewriter.getIndexAttr(shape[i]));
                 hb_outer.push_back(dimSize);
                 step_outer.push_back(one);
             }
+            lb_orig.push_back(zero);
+            hb_orig.push_back(dimSize);
+            step_orig.push_back(one);
         }
         lb.assign(lb_outer.begin(), lb_outer.end());
         hb.assign(hb_outer.begin(), hb_outer.end());
@@ -108,24 +105,47 @@ public:
         lb.push_back(zero);
         Value reduceDimSize = rewriter.create<ConstantOp>(loc, rewriter.getIndexAttr(shape[reduceDimValue]));
         hb.push_back(reduceDimSize);
-        step.push_back(one);        
+        step.push_back(one);  
+
+        // rewriter.create<linalg::FillOp>(loc, zero, nnz_per_row);
+        // rewriter.create<linalg::FillOp>(loc, dim, index_arr);
+        scf::buildLoopNest(rewriter, loc, lb_outer, hb_outer, step_outer, 
+            [&](OpBuilder &builder, Location loc, ValueRange ivs) {
+                builder.create<memref::StoreOp>(loc, zero, nnz_per_row, ivs);
+                return;
+            });
+        scf::buildLoopNest(rewriter, loc, lb_orig, hb_orig, step_orig, 
+            [&](OpBuilder &builder, Location loc, ValueRange ivs) {
+                builder.create<memref::StoreOp>(loc, dim, index_arr, ivs);
+                return;
+            });
+
+              
         scf::buildLoopNest(rewriter, loc, lb, hb, step, 
             [&](OpBuilder &builder, Location loc, ValueRange ivs) {
+                SmallVector<Value, 3> ivs_vec(lb.size());
+                ivs_vec.assign(ivs.begin(), ivs.end());
+                auto outer_ivs = llvm::makeArrayRef(ivs_vec).take_front(shape.size() - 1); // wrong size?
+                auto inner_iv = ivs_vec.pop_back_val();
+                auto inner_reduce_dim_ivs = llvm::makeArrayRef(ivs_vec).drop_back(shape.size() - reduceDimValue - 1);
+                ivs_vec.pop_back_n(shape.size() - reduceDimValue - 1);
+                
                 Value elm = builder.create<memref::LoadOp>(loc, input, ivs);
                 Value not_zero = builder.create<CmpFOp>(loc, CmpFPredicate::ONE, 
-                                    elm, zero);
+                                    elm, zeroElm);
                 builder.create<scf::IfOp>(loc, not_zero, [&](OpBuilder &b, Location loc) {
-                    SmallVector<Value, 3> ivs_vec(lb.size());
-                    ivs_vec.assign(ivs.begin(), ivs.end());
-                    auto outer_ivs = llvm::makeArrayRef(ivs_vec).take_front(shape.size());
                     Value old_nnz = b.create<memref::LoadOp>(loc, nnz_per_row, outer_ivs);
                     Value new_nnz = b.create<AddIOp>(loc, old_nnz, one);
                     b.create<memref::StoreOp>(loc, new_nnz, nnz_per_row, outer_ivs);
-                    auto inner_iv = ivs_vec.pop_back_val();
+                    
                     ivs_vec.push_back(old_nnz);
+                    for (unsigned i = 0; i < inner_reduce_dim_ivs.size(); i++) {
+                        ivs_vec.push_back(inner_reduce_dim_ivs[i]);
+                    }
+
                     ValueRange index_arr_idx = llvm::makeArrayRef(ivs_vec);
                     b.create<memref::StoreOp>(loc, inner_iv, index_arr, index_arr_idx);
-                    return;
+                    b.create<scf::YieldOp>(loc, ValueRange{});
                 });
             });
         
@@ -144,13 +164,99 @@ public:
                 Value is_row_nnz_greater = builder.create<CmpIOp>(loc, CmpIPredicate::ugt, row_nnz, tmp_max);
                 builder.create<scf::IfOp>(loc, is_row_nnz_greater, [&](OpBuilder &b, Location loc) {
                     b.create<memref::StoreOp>(loc, row_nnz, max_nnz, zero);
+                    b.create<scf::YieldOp>(loc, ValueRange{});
                 });
                 return;
             });
         
         // Allcate result arrays
         Value nnz = rewriter.create<memref::LoadOp>(loc, nnz_count, zero);
-        for 
+        MemRefType dynamicIndexType = MemRefType::get(-1, indexTp);
+        MemRefType dynamicDataType = MemRefType::get(-1, input.getType().cast<MemRefType>().getElementType());
+        // switch (padding) {
+        //     case "none":
+        SmallVector<Value, 3> idx_array;
+        Value val_array;
+        if (padding == "none") {
+            for (unsigned i = 0; i < shape.size(); i++) {
+                idx_array.push_back(rewriter.create<memref::AllocOp>(loc, dynamicIndexType, nnz));
+            }
+            val_array = rewriter.create<memref::AllocOp>(loc, dynamicDataType, nnz);
+        } else if (padding == "zero") {
+            std::vector<int64_t> ell_shape;
+            for (unsigned i = 0; i < shape.size(); i++) {
+                if (i != reduceDimValue) 
+                    ell_shape.push_back(shape[i]);
+                else
+                    ell_shape.push_back(-1);
+            }
+            MemRefType ellIndexTp = MemRefType::get(ArrayRef<int64_t>(ell_shape), indexTp);
+            MemRefType ellDataTp = MemRefType::get(ArrayRef<int64_t>(ell_shape), input.getType().cast<MemRefType>().getElementType());
+            // Value ell_array_size = rewriter.create<MulOp>(loc, max_nnz, )
+            idx_array.push_back(rewriter.create<memref::AllocOp>(loc, ellIndexTp, max_nnz));
+            val_array = rewriter.create<memref::AllocOp>(loc, ellDataTp, max_nnz);
+        } else {
+            LLVM_DEBUG(llvm::dbgs() << "The padding option in PackOp can only support 'none' and 'zero' now.");
+        }
+         
+        Value max_nnz_val = rewriter.create<memref::LoadOp>(loc, max_nnz, zero);
+        Value len_count = rewriter.create<memref::AllocaOp>(loc, MemRefType::get(ArrayRef<int64_t>({1}), indexTp));
+        rewriter.create<memref::StoreOp>(loc, zero, len_count, zero);
+        // affine_map: reorder lb, hb 
+        SmallVector<Value> lb_ordered, hb_ordered, step_ordered;
+        for (unsigned i = 0; i < shape.size(); i++) {
+            unsigned reorderedDimPos = storageOrder.getDimPosition(i);
+            LLVM_DEBUG(llvm::dbgs() << "storage order dim = " << reorderedDimPos <<
+                    " | permuted dim = " << storageOrder.getPermutedPosition(i) << "\n");
+            if (reorderedDimPos != reduceDimValue) {
+                lb_ordered.push_back(zero);
+                Value reorderedDimSize = rewriter.create<ConstantOp>(loc, rewriter.getIndexAttr(shape[reorderedDimPos]));
+                hb_ordered.push_back(reorderedDimSize);
+                step_ordered.push_back(one);
+            }
+            else {
+                lb_ordered.push_back(zero);
+                hb_ordered.push_back(max_nnz_val);
+                step_ordered.push_back(one);
+            }
+        }
+        scf::buildLoopNest(rewriter, loc, lb_ordered, hb_ordered, step_ordered, 
+            [&](OpBuilder &builder, Location loc, ValueRange ivs) {
+                Value reordered_idx = builder.create<memref::LoadOp>(loc, index_arr, ivs);
+                if (padding == "none") {
+                    Value valid_idx = builder.create<CmpIOp>(loc, CmpIPredicate::ult, reordered_idx, dim);
+                    builder.create<scf::IfOp>(loc, valid_idx, [&](OpBuilder &b, Location loc) {
+                        Value len_count_val = b.create<memref::LoadOp>(loc, len_count, zero);
+                        
+                        // prepare the reordered loading index of input A, store indices into index arrays
+                        SmallVector<Value, 3> load_dim;
+                        for (unsigned i = 0; i < shape.size(); i++) {
+                            unsigned reorderedDimPos = storageOrder.getDimPosition(i);
+                            if (reorderedDimPos != reduceDimValue) {
+                                b.create<memref::StoreOp>(loc, ivs[reorderedDimPos], idx_array[i], len_count_val);
+                                load_dim.push_back(ivs[reorderedDimPos]);
+                            } else {
+                                b.create<memref::StoreOp>(loc, reordered_idx, idx_array[i], len_count_val);
+                                load_dim.push_back(reordered_idx);
+                            }
+                        }
+                        Value a_mem_val = b.create<memref::LoadOp>(loc, input, llvm::makeArrayRef(load_dim));
+                        b.create<memref::StoreOp>(loc, a_mem_val, val_array, len_count_val);
+                        Value len_count_sum = b.create<AddIOp>(loc, len_count_val, one);
+                        b.create<memref::StoreOp>(loc, len_count_sum, len_count, zero);
+                        b.create<scf::YieldOp>(loc, ValueRange{});
+                    });
+                } else if (padding == "zero") {
+
+                } else 
+                    LLVM_DEBUG(llvm::dbgs() << "The padding option in PackOp can only support 'none' and 'zero' now.");
+            
+                // builder.create<memref::StoreOp>(loc, sum, nnz_count, zero);
+                // Value tmp_max = builder.create<memref::LoadOp>(loc, max_nnz, zero);
+                // Value is_row_nnz_greater = builder.create<CmpIOp>(loc, CmpIPredicate::ugt, row_nnz, tmp_max);
+                // Value sum = builder.create<AddIOp>(loc, row_nnz, tmp_count);
+                return;
+            });
 
         rewriter.eraseOp(op);
         // LLVM_DEBUG(llvm::dbgs() << "hello!\n"); 
