960797a68699f77567898e6a36360b3a646ceb02
ZijD <zijianding@163.com>
2022-06-19 13:48:34 -0400

    change bin-path and cmake-config

--- scripts/bin-path.sh
+++ scripts/bin-path.sh
@@ -1,3 +1,3 @@
-export LD_LIBRARY_PATH=/home/dingzj/sparse/Sparse_Layout_Dialect/build/lib:$LD_LIBRARY_PATH
-export PATH=/home/dingzj/sparse/Sparse_Layout_Dialect/build/bin:$PATH
+export LD_LIBRARY_PATH=/work/shared/users/ugrad/zd226/Sparse_Layout_Dialect/build/lib:$LD_LIBRARY_PATH
+export PATH=/work/shared/users/ugrad/zd226/Sparse_Layout_Dialect/build/bin:$PATH
 export TENSOR0=/work/shared/users/staff/zz546/temp/Sparse_Layout_Dialect/test/Data/tensor46.mtx
--- scripts/cmake-config.sh
+++ scripts/cmake-config.sh
@@ -1,8 +1,4 @@
-cmake -G Ninja -B build -Wno-dev \
--DMLIR_DIR=/home/dingzj/sparse/tmp/LLVM/build/lib/cmake/mlir \
--DLLVM_EXTERNAL_LIT=/home/dingzj/sparse/tmp/LLVM/build/bin/llvm-lit \
+cmake -G Ninja -B build \
+-DMLIR_DIR=/work/shared/common/llvm-project-gpu/build/lib/cmake/mlir \
+-DLLVM_EXTERNAL_LIT=/work/shared/common/llvm-project-gpu/build/bin/llvm-lit \
 -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ 
-
-# ninja
-# ninja check-sparlay
-# ninja mlir-doc
