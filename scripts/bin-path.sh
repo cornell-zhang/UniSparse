dc254de582af5ec9c884f1dafd3049eb9a579dae
ZijD <zijianding@163.com>
2022-06-20 01:40:43 +0800

    1) add runtime functions 2) a complete demo (see sparlay-convert.mlir)

--- scripts/bin-path.sh
+++ scripts/bin-path.sh
@@ -1,3 +1,5 @@
 export LD_LIBRARY_PATH=/home/dingzj/sparse/Sparse_Layout_Dialect/build/lib:$LD_LIBRARY_PATH
 export PATH=/home/dingzj/sparse/Sparse_Layout_Dialect/build/bin:$PATH
-export TENSOR0=/work/shared/users/staff/zz546/temp/Sparse_Layout_Dialect/test/Data/tensor46.mtx
+export TENSOR0=/home/dingzj/sparse/Storage/data/test.mtx
+export LLVMHOME=/home/dingzj/sparse/tmp/LLVM
+export SPLHOME=/home/dingzj/sparse/Sparse_Layout_Dialect
