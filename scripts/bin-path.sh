991af81d8972d951321a9e5fa8573ced67a03510
ZijD <zijianding@163.com>
2022-06-26 11:53:53 -0400

    fix attribute bug

--- scripts/bin-path.sh
+++ scripts/bin-path.sh
@@ -1,5 +1,5 @@
 export LD_LIBRARY_PATH=/work/shared/users/ugrad/zd226/Sparse_Layout_Dialect/build/lib:$LD_LIBRARY_PATH
 export PATH=/work/shared/users/ugrad/zd226/Sparse_Layout_Dialect/build/bin:$PATH
-export TENSOR0=/work/shared/users/staff/zz546/temp/Sparse_Layout_Dialect/test/Data/tensor46.mtx
+export TENSOR0=/work/shared/users/ugrad/zd226/test_data/test1.mtx
 export LLVMHOME=/work/shared/common/llvm-project-gpu
 export SPLHOME=/work/shared/users/ugrad/zd226/Sparse_Layout_Dialect
