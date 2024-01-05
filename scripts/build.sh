0f14d255cf0311fcd816f8f0a74e6e6354efe559
Sibylau <jl3952@cornell.edu>
2024-01-04 21:40:22 -0500

    script

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,12 +2,12 @@ export LLVM_ROOT=/install/llvm-project
 export CPATH=$LLVM_ROOT/openmp/build/runtime/src:$CPATH
 export LD_LIBRARY_PATH=$LLVM_ROOT/openmp/build/runtime/src:$LD_LIBRARY_PATH
 
-cmake -B build \
+mkdir -p build && cd build
+cmake .. \
 -DMLIR_DIR=$LLVM_ROOT/build/lib/cmake/mlir \
 -DLLVM_EXTERNAL_LIT=$LLVM_ROOT/build/bin/llvm-lit \
--DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ 
-
-cmake --build build/
+-DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++  && \
+cmake --build .
 
 export SPLHOME=/install/UniSparse
 export LD_LIBRARY_PATH=/install/UniSparse/build/lib:$LD_LIBRARY_PATH
