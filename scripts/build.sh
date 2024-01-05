a98397e12f5462854234ce6664614486653534e6
Sibylau <jl3952@cornell.edu>
2024-01-04 21:24:46 -0500

    scripts

---
+++ scripts/build.sh
@@ -0,0 +1,13 @@
+export LLVM_ROOT=/install/llvm-project
+
+cmake -G Ninja -B build \
+-DMLIR_DIR=$LLVM_ROOT/build/lib/cmake/mlir \
+-DLLVM_EXTERNAL_LIT=$LLVM_ROOT/build/bin/llvm-lit \
+-DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ 
+
+ninja build/
+
+export SPLHOME=/install/UniSparse
+export LD_LIBRARY_PATH=/install/UniSparse/build/lib:$LD_LIBRARY_PATH
+export PATH=/install/UniSparse/build/bin:$PATH
+
