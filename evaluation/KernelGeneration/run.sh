34ec048671a7a1465a3e16909050b8c02d001a43
Sibylau <jl3952@cornell.edu>
2024-01-06 02:21:03 -0500

    script

--- evaluation/KernelGeneration/run.sh
+++ evaluation/KernelGeneration/run.sh
@@ -15,13 +15,13 @@ unisparse-opt ./UniSparse/unisparse_csc_csc_csc_spgemm_F64.mlir -unisparse-codeg
 clang++ unisparse_csc_csc_csc_spgemm_F64.o -L$SPLHOME/build/lib -lmlir_unisparse_runner_utils -L$LLVM_ROOT/build/lib -lmlir_runner_utils -lmlir_c_runner_utils -o unisparse_csc_csc_csc_spgemm_F64
 
 mlir-opt ./sparse_tensor_dialect/sparse_tensor_csr_spmm_F64.mlir -sparse-compiler | mlir-translate -mlir-to-llvmir | opt -O3 -S | llc -O3 -relocation-model=pic -filetype=obj -o sparse_tensor_csr_spmm_F64.o
-clang++ sparse_tensor_csr_spmm_F64.o -L$SPLHOME/build/lib -lmlir_sparlay_runner_utils -L$LLVM_ROOT/build/lib -lmlir_runner_utils -lmlir_c_runner_utils -o sparse_tensor_csr_spmm_F64
+clang++ sparse_tensor_csr_spmm_F64.o -L$LLVM_ROOT/build/lib -lmlir_runner_utils -lmlir_c_runner_utils -o sparse_tensor_csr_spmm_F64
 mlir-opt ./sparse_tensor_dialect/sparse_tensor_dcsc_spmm_F64.mlir -sparse-compiler | mlir-translate -mlir-to-llvmir | opt -O3 -S | llc -O3 -relocation-model=pic -filetype=obj -o sparse_tensor_dcsc_spmm_F64.o
-clang++ sparse_tensor_dcsc_spmm_F64.o -L$SPLHOME/build/lib -lmlir_sparlay_runner_utils -L$LLVM_ROOT/build/lib -lmlir_runner_utils -lmlir_c_runner_utils -o sparse_tensor_dcsc_spmm_F64
+clang++ sparse_tensor_dcsc_spmm_F64.o -L$LLVM_ROOT/build/lib -lmlir_runner_utils -lmlir_c_runner_utils -o sparse_tensor_dcsc_spmm_F64
 mlir-opt ./sparse_tensor_dialect/sparse_tensor_csr_csr_csr_spgemm_F64.mlir -sparse-compiler | mlir-translate -mlir-to-llvmir | opt -O3 -S | llc -O3 -relocation-model=pic -filetype=obj -o sparse_tensor_csr_csr_csr_spgemm_F64.o
-clang++ sparse_tensor_csr_csr_csr_spgemm_F64.o -L$SPLHOME/build/lib -lmlir_unisparse_runner_utils -L$LLVM_ROOT/build/lib -lmlir_runner_utils -lmlir_c_runner_utils -o sparse_tensor_csr_csr_csr_spgemm_F64
+clang++ sparse_tensor_csr_csr_csr_spgemm_F64.o -L$LLVM_ROOT/build/lib -lmlir_runner_utils -lmlir_c_runner_utils -o sparse_tensor_csr_csr_csr_spgemm_F64
 mlir-opt ./sparse_tensor_dialect/sparse_tensor_csc_csc_csc_spgemm_F64.mlir -sparse-compiler | mlir-translate -mlir-to-llvmir | opt -O3 -S | llc -O3 -relocation-model=pic -filetype=obj -o sparse_tensor_csc_csc_csc_spgemm_F64.o
-clang++ sparse_tensor_csc_csc_csc_spgemm_F64.o -L$SPLHOME/build/lib -lmlir_unisparse_runner_utils -L$LLVM_ROOT/build/lib -lmlir_runner_utils -lmlir_c_runner_utils -o sparse_tensor_csc_csc_csc_spgemm_F64
+clang++ sparse_tensor_csc_csc_csc_spgemm_F64.o -L$LLVM_ROOT/build/lib -lmlir_runner_utils -lmlir_c_runner_utils -o sparse_tensor_csc_csc_csc_spgemm_F64
 
 g++ -std=c++11 -O3 -DNDEBUG -DTACO -I/install/taco/include -L/install/taco/build/lib ./taco/taco_csr_spmm.cpp -o taco_csr_spmm -ltaco
 g++ -std=c++11 -O3 -DNDEBUG -DTACO -I/install/taco/include -L/install/taco/build/lib ./taco/taco_dcsc_spmm.cpp -o taco_dcsc_spmm -ltaco
